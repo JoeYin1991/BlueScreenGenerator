c2ae61a5c948d8549b7179af1013496a249a1e6a
Joe YIN <qiang.yin@hpmicro.com>
2024-12-03 00:07:21 +0800

    support saving in appinfo

--- Generator/appinfo.cpp
+++ Generator/appinfo.cpp
@@ -1,5 +1,10 @@
 #include "appinfo.h"
 #include <QColor>
+#include <QCoreApplication>
+#include <QSettings>
+#include <QTextCodec>
+
+const QString ini_name = "config.ini";
 
 AppInfo *AppInfo::instance()
 {
@@ -9,7 +14,27 @@ AppInfo *AppInfo::instance()
 
 void AppInfo::save()
 {
-    QString path;
+    QString path = QCoreApplication::applicationDirPath() + "/" + ini_name;
+    QSettings settings(path, QSettings::IniFormat);
+    settings.setIniCodec(QTextCodec::codecForName("UTF-8"));
+
+    if (model.mEmojiType == EEmojiType::Char) {
+        settings.setValue("emoji_char", model.mEmojiCharacter);
+        settings.setValue("emoji_path", "");
+    } else {
+        settings.setValue("emoji_char", "");
+        settings.setValue("emoji_path", model.mEmojiImgPath);
+    }
+
+    settings.setValue("main_content", model.mMainContent);
+    settings.setValue("contact_hint", model.mCttHint);
+    settings.setValue("contact_info", model.mCttInfo);
+    settings.setValue("qrcode", model.mQrcodePath);
+
+    settings.setValue("background_color", QString("%1,%2,%3").arg(model.mBgColor.red()).arg(model.mBgColor.green()).arg(model.mBgColor.blue()));
+    settings.setValue("progress_time", model.progressTime);
+    settings.setValue("hot_key", model.mHotKey);
+    settings.setValue("execute_cmd", model.cmd);
 }
 
 AppInfo::AppInfo()
